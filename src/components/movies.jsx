591b1dd72fee4d8bb92dd13ae77f723a6697b963
kleber jose da silva <kleber.jose.da.silva@accenture.com>
2019-11-19 09:48:17 -0300

    function delete-movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -9,6 +9,11 @@ class Movies extends Component {
   componentDidMount() {
     this.setState({ movies: getMovies() });
   }
+
+  handleDelete(movie) {
+    const movies = this.state.movies.filter(m => m !== movie);
+    this.setState({ movies });
+  }
   render() {
     return (
       <table className="table">
@@ -24,16 +29,21 @@ class Movies extends Component {
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.numberInStock}</td>
               <td>
-                <i className="fa fa-heart-o"></i>
+                <i onLike={this.handleLike} className="fa fa-heart-o"></i>
               </td>
               <td>
-                <button className="btn btn-danger btn-sm m-2">Delete</button>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm m-2"
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
