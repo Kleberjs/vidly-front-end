799436e84d39f60709df6becc73b44e9476b9879
kleber jose da silva <kleber.jose.da.silva@accenture.com>
2019-11-19 13:53:50 -0300

    create paginate component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "./../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
-    movies: []
+    movies: [],
+    pageItem: 4,
+    currentPage: 1
   };
 
   componentDidMount() {
@@ -24,41 +27,59 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  handlePaginate = currentPage => {
+    this.setState({ currentPage });
+  };
+
   render() {
+    const {
+      movies: { length: countMovies },
+      pageItem,
+      currentPage
+    } = this.state;
+
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like onLike={this.handleLike} item={movie} />
-              </td>
-              <td>
-                <button
-                  onClick={() => this.handleDelete(movie)}
-                  className="btn btn-danger btn-sm m-2"
-                >
-                  Delete
-                </button>
-              </td>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like onLike={this.handleLike} item={movie} />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm m-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          totalItem={countMovies}
+          itemPage={pageItem}
+          currentPage={currentPage}
+          onPaginate={this.handlePaginate}
+        />
+      </div>
     );
   }
 }
