5ab77e4208e50858026071fe494dbdef99c798e1
kleber jose da silva <kleber.jose.da.silva@accenture.com>
2019-11-25 23:44:06 -0300

    create TableMovies component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,12 @@
+
+/**
+ * IMPLEMENTAR O ORDENAMENTO POR: title, genre, stock or rate
+ */
+
 import React, { Component } from "react";
+import TableMovies from "./tableMovies";
 import { getMovies } from "./../services/fakeMovieService";
 import { getGenres } from "./../services/fakeGenreService";
-import Like from "./common/like";
 import GroupList from "./common/groupList";
 import Pagination from "./common/pagination";
 import paginate from "./../utils/paginate";
@@ -75,39 +80,7 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>There is {filtered.length} movies in the database</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map(movie => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like onLike={this.handleLike} item={movie} />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm m-2"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <TableMovies movies={movies} onLike={this.handleLike} onDelete={this.handleDelete} />
           <Pagination
             totalItem={filtered.length}
             itemPage={pageItem}
