a494e95798cb9119b4203692e61c1c36fd380ebd
kleber jose da silva <kleber.jose.da.silva@accenture.com>
2019-11-26 13:37:19 -0300

    added arrow icon in the headers

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-// columns: array
-// currentSort: object
-// onSort: function
-
+// <i class="fa fa-chevron-up" aria-hidden="true"></i>
 const TableHeader = ({ columns, currentSort, onSort }) => {
   const raiseSort = path => {
     if (currentSort.path === path)
@@ -15,15 +12,32 @@ const TableHeader = ({ columns, currentSort, onSort }) => {
 
     onSort(currentSort); // herdado
   };
+
+  const handleIcon = (currentSort, path) => {
+    if (currentSort.path === path) {
+      return currentSort.order === "asc" ? (
+        <i class="fa fa-chevron-up" aria-hidden="true"></i>
+      ) : (
+        <i class="fa fa-chevron-down" aria-hidden="true"></i>
+      );
+    }
+
+    return null;
+    // <i class="fa fa-chevron-up" aria-hidden="true"></i>
+  };
+  const icon = <i className="fa fa-chevron-up"></i>;
+
   return (
     <thead>
       <tr>
         {columns.map(column => (
           <th
+            style={{ cursor: "pointer" }}
             key={column.path || column.key}
             onClick={() => raiseSort(column.path)}
           >
             {column.name}
+            {handleIcon(currentSort, column.path)}
           </th>
         ))}
       </tr>
